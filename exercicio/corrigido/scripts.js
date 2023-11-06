538ae61c090ebfc3d29171e3954acc21c812d9cf
Victoria Santos <victoriasantos@MacBook-Pro-de-Victoria.local>
2023-11-06 22:38:07 +0000

    Corrigir estrutura e organização

--- exercicio/corrigido/scripts.js
+++ exercicio/corrigido/scripts.js
@@ -1,3 +1,72 @@
+
+const languageButton = document.getElementById('language-button');
+const languageOption = document.getElementById('language-option');
+const engButton = document.getElementById('eng-button');
+
+
+engButton.addEventListener('click', () => {
+
+  if (languageOption.textContent === 'PT') {
+    languageOption.textContent = 'EN';
+  } else {
+    languageOption.textContent = 'PT';
+  }
+  
+  
+  const languageOptions = document.querySelectorAll('.language-option');
+  languageOptions.forEach(option => option.classList.remove('selected'));
+  languageOption.classList.add('selected');
+});
+
+languageButton.addEventListener('keydown', (event) => {
+  if (event.key === 'Enter') {
+    languageButton.click();
+  }
+});
+
+
+document.getElementById("openModalButton").addEventListener("click", function () {
+  document.getElementById("searchModal").style.display = "flex";
+});
+
+document.getElementById("closeModalButton").addEventListener("click", function () {
+  document.getElementById("searchModal").style.display = "none";
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const slides = document.querySelectorAll(".slide");
+  const prevButton = document.querySelector(".prev-slide");
+  const nextButton = document.querySelector(".next-slide");
+
+  let currentIndex = 0;
+
+  // Function to show the current slide and hide others
+  function showCurrentSlide() {
+      slides.forEach((slide, index) => {
+          if (index === currentIndex) {
+              slide.classList.add("active");
+          } else {
+              slide.classList.remove("active");
+          }
+      });
+  }
+
+  // Event listener for the previous button
+  prevButton.addEventListener("click", function () {
+      currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+      showCurrentSlide();
+  });
+
+  // Event listener for the next button
+  nextButton.addEventListener("click", function () {
+      currentIndex = (currentIndex + 1) % slides.length;
+      showCurrentSlide();
+  });
+
+  // Initialize the slider
+  showCurrentSlide();
+});
+
 document.addEventListener("DOMContentLoaded", function () { 
     // Event listener for Close Submenu
     document.addEventListener('keydown', (event) => {
@@ -8,4 +77,20 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
     
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const openButton = document.getElementById('openSearchModalButton');
+  const modal = document.getElementById('searchModal');
+  const closeButton = document.getElementById('closeSearchModalButton');
+  const searchInput = document.getElementById('searchInput');
+
+  openButton.addEventListener('click', () => {
+      modal.style.display = 'flex';
+      searchInput.focus();
+  });
+
+  closeButton.addEventListener('click', () => {
+      modal.style.display = 'none';
+  });
 });
